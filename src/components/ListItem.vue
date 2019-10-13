60449ea2f065d0094d9d215226b6a543f37a8e39
Yoonseop Kang <e0engoon@gmail.com>
2019-10-13 15:09:31 +0900

    Add ListItem component

---
+++ src/components/ListItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li class="todo-list-item">
+    <input type="checkbox" />
+    <span class="content">{{ content }}</span>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    content: String,
+    checked: Boolean
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,15 +2,22 @@
 <div class="todo-list">
   <h1>{{ msg }}</h1>
   <ul>
+    <ListItem v-for="i in 10" :key="i" :content="i"/>
   </ul>
 </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import ListItem from '@/components/ListItem.vue'
+
 export default {
   name: 'TodoList',
   props: {
     msg: String
+  },
+  components: {
+    ListItem
   }
 }
 </script>

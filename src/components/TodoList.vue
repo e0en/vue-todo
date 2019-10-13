33cdac82e5defd69ae718273e2d7c5fc519dcc35
Yoonseop Kang <e0engoon@gmail.com>
2019-10-13 14:04:42 +0900

    Delete boilerplate contents

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="todo-list">
+  <h1>{{ msg }}</h1>
+  <ul>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todo_list">
+    <TodoList msg="Todo List"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TodoList from '@/components/TodoList.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    TodoList
   }
 }
 </script>

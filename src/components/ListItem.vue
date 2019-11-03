6ff1ba8d9947fdd4155364ca9d09100cdcf38e6d
Yoonseop Kang <e0engoon@gmail.com>
2019-11-03 18:50:48 +0900

    Add edit functionality

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -2,7 +2,9 @@
   <li class="todo-list-item" :class="{ complete: checked }">
     <input type="checkbox" v-model="checked" v-on:change="update" />
     <input type="hidden" :value="itemId" />
-    <span class="content">{{ content }}</span>
+    <span class="content" v-on:click="showEdit" v-bind:class="[isEditing ?  'hidden' : '']">{{ content }}</span>
+    <input ref="text" type="text" v-model="text" v-bind:class="[isEditing ? '' : 'hidden']"
+      v-on:blur="editItem" v-on:keyup.enter="editItem" />
     <button class="delete" v-on:click="deleteItem">delete</button>
   </li>
 </template>
@@ -17,7 +19,9 @@ export default {
   },
   data: function () {
     return {
-      checked: this.isComplete
+      checked: this.isComplete,
+      text: this.content,
+      isEditing: false
     }
   },
   methods: {
@@ -31,6 +35,23 @@ export default {
     },
     deleteItem: function () {
       this.$emit('deleteItem', this.itemId)
+    },
+    showEdit: function () {
+      this.isEditing = true
+      this.$nextTick(() => {
+        console.log(this.$refs.text.focus())
+      })
+    },
+    editItem: function () {
+      if (this.isEditing) {
+        var ev = {
+          itemId: this.itemId,
+          content: this.text,
+          checked: this.checked
+        }
+        this.$emit('update', ev)
+        this.isEditing = false
+      }
     }
   }
 }
@@ -46,5 +67,8 @@ input[type=checkbox] {
 }
 button.delete {
   margin-left: 1em;
+}
+.hidden {
+  display: none;
 }
 </style>
